5767511212df44af486ef122d679c4121965ab1e
NickBowers <106525095+NickBowers@users.noreply.github.com>
2022-05-30 11:16:09 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit
 
 streamlit.title("My Mom's New Healthy Diner")
 
+
 streamlit.header('Breakfast Favourites')
-streamlit.text('streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
